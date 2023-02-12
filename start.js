5e8fd42f40a2620440a3864ffad50ab984040f27
Michael Kreil <github@michael-kreil.de>
2023-02-12 14:04:01 +0100

    change folder names

--- start.js
+++ start.js
@@ -18,8 +18,11 @@ async function start() {
 		tiles_url: 'https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/tiles/v1/bm_web_de_3857/{z}/{x}/{y}.pbf',
 	}
 
-	CONFIG.tiles_folder = resolve(__dirname, "cache", CONFIG.name, 'tiles');
-	CONFIG.geojson_folder = resolve(__dirname, "cache", CONFIG.name, 'geojson');
+	'1_tiles,2_geojson,3_gpkg,4_geojson,5_gpkg'.split(',').forEach(name => {
+		let folder = resolve(__dirname, "cache", CONFIG.name, name);
+		mkdirSync(folder, { recursive: true });
+		CONFIG['folder'+name] = folder;
+	})
 
 	await scrape_tiles(CONFIG);
 	await merge_tiles_to_layers(CONFIG);
